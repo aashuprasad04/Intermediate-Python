3b6ae101f83e299a082c127330a9ba606860cc22
Aashu Prasad <aashuprasad047@gmail.com>
2026-03-11 12:03:43 +0530

    Update map.py

--- Lambda & Functional Programming/map.py
+++ Lambda & Functional Programming/map.py
@@ -9,3 +9,8 @@ print(tuple(result))
 # map(function, iterable) applies the function to each element.
 # It returns a map object (iterator). (result is a map object)
 # You usually convert it using list(), tuple(), or loop through it.
+
+nums = [97, 98, 99]
+result = list(map(chr, nums))
+
+print(type(result), result)
