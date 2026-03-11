de50e54af9a5257b35693710ed26a894b1c01dad
Aashu Prasad <aashuprasad047@gmail.com>
2026-03-11 12:00:27 +0530

    Create map.py

---
+++ Lambda & Functional Programming/map.py
@@ -0,0 +1,11 @@
+# add = lambda x: [i+1 for i in x]
+nums = [1,2,3,4,5]
+# print(add(nums))  # output : [2, 3, 4, 5, 6]
+
+result = map(lambda x: x+1, nums)
+print(tuple(result))
+
+
+# map(function, iterable) applies the function to each element.
+# It returns a map object (iterator). (result is a map object)
+# You usually convert it using list(), tuple(), or loop through it.
