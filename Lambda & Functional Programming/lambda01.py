f7d8ceff2da448c91960b8898a394bce16d0f4ed
Aashu Prasad <aashuprasad047@gmail.com>
2026-03-11 11:22:01 +0530

    Create lambda01.py

---
+++ Lambda & Functional Programming/lambda01.py
@@ -0,0 +1,14 @@
+add= lambda a,b : a+b
+print(add(5,6))
+
+# lambda fun is a single line fun without a name, create using the lambda keyword
+
+# add= lambda a,b : a+b
+# add = fun name
+# lambda = keyword
+# a,b = parameters
+# a+b = statement
+
+
+Aprint = lambda a: print(a)
+Aprint("azy")
