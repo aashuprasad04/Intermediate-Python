b1bfd45eddde937c254e99562576c6debe9c08ea
Aashu Prasad <aashuprasad047@gmail.com>
2026-03-20 18:12:16 +0000

    Create pipProb.py

---
+++ CodeWithHarry/Chapter01/pipProb.py
@@ -0,0 +1,10 @@
+# sudo apt install python3-venv
+# python3 -m venv myenv
+# source myenv/bin/activate
+
+# now setup are complete 
+# install pip
+
+# pip install pyttsx3
+
+# also run pip in a environement
